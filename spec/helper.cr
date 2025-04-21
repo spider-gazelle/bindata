bebf9df0fcf9ce421fb396230c20915b23b44c91
Michael Nikitochkin <michael.nikitochkin@gmx.net>
2025-04-21 18:12:00 +0200

    crystal tool format with Crystal v1.16

--- spec/helper.cr
+++ spec/helper.cr
@@ -5,47 +5,47 @@ require "../src/bindata/asn1"
 class Header < BinData
   endian little
 
-  field size : Int32, value: ->{ name.bytesize }
-  field name : String, length: ->{ size }
+  field size : Int32, value: -> { name.bytesize }
+  field name : String, length: -> { size }
 end
 
 class Body < BinData
   endian big
 
-  field start : UInt8, value: ->{ 0_u8 }
+  field start : UInt8, value: -> { 0_u8 }
 
-  bit_field onlyif: ->{ start == 0 } do
-    bits 6, :six, value: ->{ 0b1110_11_u8 }
+  bit_field onlyif: -> { start == 0 } do
+    bits 6, :six, value: -> { 0b1110_11_u8 }
     # bits 3, :three, default: 0b011
     bits 3, three = 0b011
-    bits 4, four, value: ->{ 0b1001_u8 }
-    bits 11, :teen, value: ->{ 0b1101_1111_101_u16 }
+    bits 4, four, value: -> { 0b1001_u8 }
+    bits 11, :teen, value: -> { 0b1101_1111_101_u16 }
   end
 
-  field mid : UInt8, value: ->{ 0_u8 }
+  field mid : UInt8, value: -> { 0_u8 }
 
   bit_field do
-    bits 52, :five, value: ->{ 0xF0_E0_D0_C0_B0_A0_9_u64 }
-    bits 12, :eight, value: ->{ 0x104_u16 }
+    bits 52, :five, value: -> { 0xF0_E0_D0_C0_B0_A0_9_u64 }
+    bits 12, :eight, value: -> { 0x104_u16 }
   end
 
-  field end : UInt8, value: ->{ 0_u8 }
+  field end : UInt8, value: -> { 0_u8 }
 end
 
 class Wow < BinData
   endian big
 
-  field start : UInt8, value: ->{ 0_u8 }
+  field start : UInt8, value: -> { 0_u8 }
 
   # this is a shortcut for the `Header < BinData` class
   header :head
 
-  group :body, onlyif: ->{ head.size > 0 } do
-    field start : UInt8, value: ->{ 1_u8 }, onlyif: ->{ parent.start == 0 }
-    field end : UInt8, value: ->{ 3_u8 }
+  group :body, onlyif: -> { head.size > 0 } do
+    field start : UInt8, value: -> { 1_u8 }, onlyif: -> { parent.start == 0 }
+    field end : UInt8, value: -> { 3_u8 }
   end
 
-  field end : UInt8, value: ->{ 0_u8 }
+  field end : UInt8, value: -> { 0_u8 }
 end
 
 class EnumData < BinData
@@ -57,7 +57,7 @@ class EnumData < BinData
     HDMI2
   end
 
-  field start : UInt8, value: ->{ 0_u8 }
+  field start : UInt8, value: -> { 0_u8 }
   field inputs : Inputs = Inputs::HDMI
 
   bit_field do
@@ -66,7 +66,7 @@ class EnumData < BinData
     bits 2, input : Inputs = Inputs::HDMI2
   end
 
-  field end : UInt8, value: ->{ 0_u8 }
+  field end : UInt8, value: -> { 0_u8 }
 end
 
 class Inherited < EnumData
@@ -99,17 +99,17 @@ end
 class ArrayData < BinData
   endian big
 
-  field flen : UInt8 = 1, value: ->{ first.size }
-  field first : Array(Int16) = [15_i16], length: ->{ flen }
-  field slen : UInt8, value: ->{ 0_u8 | second.size }
-  field second : Array(Int8), length: ->{ slen }
+  field flen : UInt8 = 1, value: -> { first.size }
+  field first : Array(Int16) = [15_i16], length: -> { flen }
+  field slen : UInt8, value: -> { 0_u8 | second.size }
+  field second : Array(Int8), length: -> { slen }
 end
 
 class VariableArrayData < BinData
   endian big
 
   field total_size : UInt8
-  field test : Array(Int8), read_next: ->{
+  field test : Array(Int8), read_next: -> {
     # Will continue reading data into the array until
     #  the array size + 2 buffer bytes equals the total size
     (test.size + 2) < total_size
@@ -121,15 +121,15 @@ class VerifyData < BinData
   endian big
 
   field size : UInt8
-  field bytes : Bytes, length: ->{ size }
-  field checksum : UInt8, verify: ->{ checksum == bytes.reduce(0) { |acc, i| acc + i } }
+  field bytes : Bytes, length: -> { size }
+  field checksum : UInt8, verify: -> { checksum == bytes.reduce(0) { |acc, i| acc + i } }
 end
 
 class RemainingBytesData < BinData
   endian big
 
   field first : UInt8
-  remaining_bytes :rest, onlyif: ->{ first == 0x02 }, verify: ->{ rest.size % 2 == 0 }
+  remaining_bytes :rest, onlyif: -> { first == 0x02 }, verify: -> { rest.size % 2 == 0 }
 end
 
 class ObjectIdentifier < BinData
